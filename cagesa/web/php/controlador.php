3d5ab3c9b9647359caa1e669015c484872b9adef
fmentado <fmentado.yahoo.es>
2014-12-08 09:04:17 +0000

    Commit inicial FMentado

---
+++ cagesa/web/php/controlador.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francisco Mentado Manzanares
+ * Date: 08/12/2014
+ * Time: 8:32
+ */
+@require_once "modelo.php";
+define("KEY_PASS", encode("kndJM"));
+@require_once "vista.php";
+?>
---
+++ cagesa/web/php/modelo.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francisco Luis Mentado
+ * Date: 08/12/2014
+ * Time: 8:40
+ */
+//////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCIONES ENCRIPTACION
+//////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Codifica la cadena de entrada al sistema base64.
+ * @param $cadena tipo String cadena a codificar.
+ * @return string cadena codificada.
+ */
+function encode($cadena)
+{
+    return base64_encode($cadena);
+}
+
+
+/**
+ * Decodifica la cadena de entrada al sistema base64.
+ * @param $cadena tipo string cadena a decodificar.
+ * @return string Cadena decodificada.
+ */
+function decode($cadena)
+{
+    return base64_decode($cadena);
+}
+
+?>
